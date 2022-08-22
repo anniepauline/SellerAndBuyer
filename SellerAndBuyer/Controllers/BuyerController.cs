691d6f189d2abcfb90446d12fcb3b1b9c5fdcab1
paulabhishek <1998vabhishekpaul@gmail.com>
2022-08-21 23:45:17 -0400

    Added new BuyerController and view for it.

---
+++ SellerAndBuyer/Controllers/BuyerController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SellerAndBuyer.Data;
+using SellerAndBuyer.Models;
+
+namespace SellerAndBuyer.Controllers
+{
+    public class BuyerController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public BuyerController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Buyer> objSellerList = _db.Buyer;
+            return View(objSellerList);
+        }
+    }
+}
